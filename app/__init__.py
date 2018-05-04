ee3d178ec4bae3279d8b0419fd951f0fa49104ae
wuyuhang <wuyuhang@julong.cc>
2018-05-04 16:57:24 +0800

    commit THE THINGS

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask,render_template
 from app.main import main
 from app.info import info
 from flask_bootstrap import  Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='hellow'
 bootstrap = Bootstrap(app)
+db = SQLAlchemy()
 @app.route('/')
 def index():
     return render_template('index.html'),200
