2e2844662925c9a34cc94deb42ab3237337141a6
wuyuhang <wuyuhang@julong.cc>
2018-05-09 18:22:54 +0800

    数据库简单操作

--- app/__init__.py
+++ app/__init__.py
@@ -18,25 +18,50 @@ class Article(db.Model):#继承db.Model
     content = db.Column(db.Text,nullable=False)
 db.create_all() #执行
 
-#
-# conn = pymysql.Connect(host='127.0.0.1',port=3307,user='root',passwd='welcome',db='pythontest',charset='utf8')
-# cursor = conn.cursor()
-# sql = 'select * from person'
-# cursor.execute(sql)
-# # rs = cursor.fetchone()
-# # print("rs:",rs)
-# #
-# # for each in cursor.fetchmany(2):
-# #     print(each)
-# # print()
-# for each in cursor.fetchall():
-#     print(each)
+
 
 
 
 @app.route('/')
 def index():
 
+    # # 增加：
+    # article1 = Article(title='aaa', content='bbb')
+    # db.session.add(article1)
+    # # 事务
+    # db.session.commit()
+
+    # # 查
+    # # select * from article where article.title='aaa';
+    # article1 = Article.query.filter(Article.title == 'aaa').first()
+    #
+    # print ('title:%s' % article1.title)
+    # print('content:%s' % article1.content)
+    article1 = Article.query.all()
+    for u in article1:
+
+        print(u.id, u.title, u.content)
+
+    # # 改：
+    # # 1. 先把你要更改的数据查找出来
+    # article1 = Article.query.filter(Article.title == 'aaa').first()
+    # # 2. 把这条数据，你需要修改的地方进行修改
+    # article1.title = 'new title'
+    # # 3. 做事务的提交
+    # db.session.commit()
+
+
+    # # 删
+    # # 1. 把需要删除的数据查找出来
+    # article1 = Article.query.filter(Article.content == 'bbb').first()
+    # # 2. 把这条数据删除掉
+    # db.session.delete(article1)
+    # # 3. 做事务提交
+    # db.session.commit()
+
+    # # 自定义
+    # ret = db.session.execute('select * from article')
+    # print (ret)
     return render_template('index.html'),200
 
 app.register_blueprint(main,url_prefix='/hello')
